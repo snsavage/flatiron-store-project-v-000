7013d3fffd18002c85fde718e2374cade3d48602
Scott Savage <snsavage@gmail.com>
2017-02-09 11:00:55 -0500

    Pass some carts and controller specs

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,19 @@
 class CartsController < ApplicationController
+  def show
+  end
 
+  def checkout
+    @cart = Cart.find(params[:id])
+
+    @cart.line_items.each do |line_item|
+      item = line_item.item
+      item.inventory -= line_item.quantity
+      item.save
+    end
+
+    current_user.current_cart = nil
+    binding.pry
+
+    redirect_to cart_path(@cart)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def display_currency(number)
     number_to_currency(number.to_f/100)
   end
+
+  def current_cart
+    current_user.current_cart if current_user
+  end
 end
